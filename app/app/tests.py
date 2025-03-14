002e33af7fa116e1677774ec25cc0472a322bfb8
soluwaloju <seun.oluwaloju@siriusxm.com>
2025-03-14 13:49:14 -0500

    SETUP-01: create django core app

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,15 +5,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-class CalcTests(SimpleTestCase):
 
+class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         response = calc.add(5, 6)
-
         self.assertEquals(response, 11)
 
     def test_subtract_numbers(self):
         response = calc.subtract(10, 15)
-
         self.assertEquals(-5, response)
